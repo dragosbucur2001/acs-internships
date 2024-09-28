fdf1557af118ba060d60f350d0da58fa58ba2566
Bucur Alexandru Dragos <dragos.bucur1105@gmail.com>
2024-09-29 01:03:17 +0300

    niste progres

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.html", "./src/styles.css"],
   theme: {
     extend: {},
   },
